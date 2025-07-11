4e1dea4579524c36f943294aa3b6252cf6624e01
Cristian Hernandez <cristianhernandez301205@gmail.com>
2025-07-11 10:18:44 -0500

    Agrego

--- yecy_cosmetic/urls.py
+++ yecy_cosmetic/urls.py
@@ -5,7 +5,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import JsonResponse
+
 urlpatterns = [
+    path("", lambda request: JsonResponse({"mensaje": "¡Bienvenido a la API de YE&CY COSMETIC!"})),
     path("admin/", admin.site.urls),
     path("api/", include("tienda.urls")),
 ]
